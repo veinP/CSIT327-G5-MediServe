b9e49c7df651cafb2377de59d5bf1e7f850ddab2
veinP <veincarmell.pangilinan@cit.edu>
2025-10-27 18:44:05 +0800

    tech(announcements): database integration

--- MediServe/apps/announcements/models.py
+++ MediServe/apps/announcements/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Announcement(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    date_posted = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'tblannouncements'
+
+    def __str__(self):
+        return self.title
--- MediServe/apps/announcements/urls.py
+++ MediServe/apps/announcements/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.announcements_view, name='announcements'),
+    path('', views.announcements_view, name='announcements'),  # admin can add/edit/delete
+    path('edit/<int:post_id>/', views.edit_post, name='edit_post'),
+    path('delete/<int:post_id>/', views.delete_post, name='delete_post'),
+    path('view-announcements/', views.view_announcements, name='view_announcements'),  # users can only view
+    path('add/', views.add_post, name='add_post'),
 ]
