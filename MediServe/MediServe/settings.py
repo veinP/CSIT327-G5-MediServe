0ed3fa5557961bf9eedb340c7a427f1b27ad0924
veinP <veincarmell.pangilinan@cit.edu>
2025-11-15 10:25:34 +0800

    Update settings.py

--- MediServe/MediServe/settings.py
+++ MediServe/MediServe/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 
 # Static files (WhiteNoise)
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = [BASE_DIR / 'MediServe' / 'static']  # Point to MediServe/static
 STATIC_ROOT = BASE_DIR / 'staticfiles'  # For production
 
 # WhiteNoise configuration
